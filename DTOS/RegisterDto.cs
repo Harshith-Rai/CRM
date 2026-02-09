a9ad4b7ddf12c6a0a89ebd890d6d6303a104842e
Harshith-Rai <Harshith.Rai@mrisoftware.com>
2026-02-09 14:58:31 +0530

    jwt initialized

---
+++ DTOS/RegisterDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRM.DTOS
+{
+    public class RegisterDto
+    {
+        [Required]
+        [EmailAddress]
+        public String Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+
+        [Required]
+        public String FullName { get; set; }
+    }
+}
---
+++ Services/JwtService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using CRM.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace CRM.Services
+
+{
+    public class JwtService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public String GenerateToken(ApplicationUser user, IList<String> Roles)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier,user.Id),
+                new Claim(ClaimTypes.Email,user.Email),
+            };
+
+            foreach (var role in Roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["jwt:key"]));
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddHours(2),
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256))
+                ;
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+
+    }
+}
