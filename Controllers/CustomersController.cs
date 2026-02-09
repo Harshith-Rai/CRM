1852cfbc5ce24daf880c4de04034efffdd3fb001
Vishwathma-MRI <vishwathma.nooji@mrisoftware.com>
2026-02-09 15:12:20 +0530

    Added the controller (CustomerController and DashBoardController)

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,41 @@
+﻿using CRM.Data;
+using CRM.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRM.Controllers
+{
+    public class CustomersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public CustomersController(AppDbContext context) { _context = context; }
+
+        [HttpGet]
+        //Read All
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _context.Customers.ToListAsync());
+        }
+        [HttpPost]
+        //CREATE
+        public async Task<IActionResult> Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetAll), new { id = customer.Id }, customer);
+        }
+
+        [HttpDelete("{id}")] // DELETE (Admin only)
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null) return NotFound();
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+    }
+}
