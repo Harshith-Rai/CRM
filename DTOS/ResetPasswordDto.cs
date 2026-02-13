8fe243e78f439b5dc88767c9a24000d0a3524094
Vishwathma-MRI <vishwathma.nooji@mrisoftware.com>
2026-02-13 16:44:35 +0530

    Fix:Forgot password issue fixed

---
+++ DTOS/ResetPasswordDto.cs
@@ -0,0 +1,20 @@
+﻿// CRM/DTOS/ResetPasswordDto.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace CRM.DTOS
+{
+    public class ResetPasswordDto
+    {
+        [Required]
+        public string Token { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, MinLength(6)]
+        public string Password { get; set; }
+
+        [Required, Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
